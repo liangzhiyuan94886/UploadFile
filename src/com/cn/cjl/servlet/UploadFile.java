2ac08be43b024c8f5f6f2f763e7d8bd77adf4796
liangzhiyuan94886 <1053813314@qq.com>
2020-07-13 00:09:02 +0800

    Signed-off-by: liangzhiyuan94886 <1053813314@qq.com>

---
+++ src/com/cn/cjl/servlet/UploadFile.java
@@ -0,0 +1,95 @@
+package com.cn.cjl.servlet;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
+/**
+ * Servlet implementation class UploadFile
+ */
+public class UploadFile extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	 // 上传文件存储目录
+    private static final String UPLOAD_DIRECTORY = "upload";
+ 
+    // 上传配置
+    private static final int MEMORY_THRESHOLD   = 1024 * 1024 * 3;  // 3MB
+    private static final int MAX_FILE_SIZE      = 1024 * 1024 * 40; // 40MB
+    private static final int MAX_REQUEST_SIZE   = 1024 * 1024 * 50; // 50MB
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UploadFile() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// 配置上传参数
+        DiskFileItemFactory factory = new DiskFileItemFactory();
+        // 设置内存临界值 - 超过后将产生临时文件并存储于临时目录中
+        factory.setSizeThreshold(MEMORY_THRESHOLD);
+        // 设置临时存储目录
+        factory.setRepository(new File(System.getProperty("java.io.tmpdir")));
+		 ServletFileUpload upload = new ServletFileUpload(factory);
+		// 设置最大文件上传值
+        upload.setFileSizeMax(MAX_FILE_SIZE);
+         
+        // 设置最大请求值 (包含文件和表单数据)
+        upload.setSizeMax(MAX_REQUEST_SIZE);
+        String uploadPath ="E:\\uplode\\"; 
+        // 中文处理
+        upload.setHeaderEncoding("UTF-8");
+        try {
+            // 解析请求的内容提取文件数据
+            @SuppressWarnings("unchecked")
+            List<FileItem> formItems = upload.parseRequest(request);
+ 
+            if (formItems != null && formItems.size() > 0) {
+                // 迭代表单数据
+                for (FileItem item : formItems) {
+                    // 处理不在表单中的字段
+                    if (!item.isFormField()) {
+                        String fileName = new File(item.getName()).getName();
+                        String filePath = uploadPath + File.separator + fileName;
+                        File storeFile = new File(filePath);
+                        // 在控制台输出文件的上传路径
+                        System.out.println(filePath);
+                        // 保存文件到硬盘
+                        item.write(storeFile);
+                        request.setAttribute("message",
+                            "文件上传成功!");
+                    }
+                }
+            }
+        } catch (Exception ex) {
+            request.setAttribute("message",
+                    "错误信息: " + ex.getMessage());
+        }
+		
+	}
+
+}
